ba919db2f518a92316b5b7d273043e4488e068f3
azim622 <fazleazim622@gmail.com>
2025-04-25 22:30:39 +0600

    added new function

--- src/pages/home/heroSection/HeroSection.jsx
+++ src/pages/home/heroSection/HeroSection.jsx
@@ -2,8 +2,8 @@ import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
-    <section className="bg-sky-100 dark:bg-gray-900 py-16 px-4">
-      <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-10 items-center">
+    <section className="bg-sky-100 dark:bg-gray-900 py-16 mb-10 rounded-xl px-4">
+      <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-10  items-center">
         <motion.div
           initial={{ opacity: 0, x: -50 }}
           whileInView={{ opacity: 1, x: 0 }}
@@ -28,9 +28,12 @@ const HeroSection = () => {
             Our Historical Artifact Tracker is your digital gateway to exploring the rich stories behind ancient objects, cultural relics, and timeless pieces of history.
             Whether you're a student, researcher, or enthusiast — we make discovering history interactive and enjoyable.
           </p>
-          <button className="px-6 py-2 text-white bg-blue-600 hover:bg-blue-700 transition rounded-full font-medium">
+          <a
+            href="/allArtifacts"
+            className="inline-block px-5 py-2 rounded-md bg-white text-teal-600 font-medium hover:bg-teal-600 hover:text-white transition"
+          >
             Explore Now
-          </button>
+          </a>
         </motion.div>
       </div>
     </section>
